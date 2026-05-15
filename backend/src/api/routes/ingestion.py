bf802cfe667c1ef66c66a5baa9124a39876c7a95
hanishkarveti <hanishkarveti@gamil.com>
2026-05-15 16:17:54 +0530

    rag1

--- backend/src/api/routes/ingestion.py
+++ backend/src/api/routes/ingestion.py
@@ -1,10 +1,41 @@
-from fastapi import APIRouter, UploadFile, File, Form, HTTPException
+from fastapi import APIRouter, UploadFile, File
+import shutil
+import os
 
-from src.services.ingestion.ingestion import split_document
+from src.services.ingestion.ingestion import (
+    load_pdf,
+    load_docx,
+    split_documents
+)
 
 router = APIRouter()
 
-@router.get("/ingest")
-def ingest_document(file: UploadFile = File(...), user_id: str = Form("anonymos")):
-    response = split_document()
-    return response
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+@router.post("/ingest")
+async def ingest_document(file: UploadFile = File(...)):
+
+    file_path = f"{UPLOAD_FOLDER}/{file.filename}"
+
+
+    if file.filename.endswith(".pdf"):
+        documents = load_pdf(file_path)
+
+    elif file.filename.endswith(".docx"):
+        documents = load_docx(file_path)
+
+    else:
+        return {
+            "error": "Unsupported file type"
+        }
+
+    chunks = split_documents(documents)
+
+    return {
+        "filename": file.filename,
+        "total_chunks": len(chunks),
+        "status": "Document chunked successfully"
+    }
--- backend/src/services/ingestion/ingestion.py
+++ backend/src/services/ingestion/ingestion.py
@@ -1,4 +1,42 @@
-def split_document():
-    return{
-        "status":"the document is splitted successfully"
-    }
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    Docx2txtLoader
+)
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def load_pdf(file_path):
+
+    loader = PyPDFLoader(file_path)
+
+    documents = loader.load()
+
+    return documents
+
+
+def load_docx(file_path):
+
+    loader = Docx2txtLoader(file_path)
+
+    documents = loader.load()
+
+    return documents
+
+
+def split_documents(documents):
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=200,
+        chunk_overlap=20
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    for index, chunk in enumerate(chunks):
+
+        print(f"CHUNK NUMBER : {index + 1}")
+
+        print(chunk.page_content)
+
+        print("\n")
+    return chunks
